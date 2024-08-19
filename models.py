e8794e0d163992a8a54174ec4b443731b93c9f9a
Alper Balbay <itsc0508@gmail.com>
2024-08-20 01:24:35 +0300

    added shape comments to the attention heads

--- models.py
+++ models.py
@@ -9,9 +9,9 @@ class Head(nn.Module):
     def __init__(self, head_size, n_embd, block_size, dropout=0.2):
         super().__init__()
         self.head_size = head_size
-        self.key = nn.Linear(n_embd, head_size, bias=False)
-        self.query = nn.Linear(n_embd, head_size, bias=False)
-        self.value = nn.Linear(n_embd, head_size, bias=False)
+        self.key = nn.Linear(n_embd, head_size, bias=False) # (head_size, n_embd)
+        self.query = nn.Linear(n_embd, head_size, bias=False) # (head_size, n_embd)
+        self.value = nn.Linear(n_embd, head_size, bias=False) # (head_size, n_embd)
         self.register_buffer("tril", torch.tril(torch.ones(block_size, block_size)))
     
         self.dropout = nn.Dropout(dropout)
