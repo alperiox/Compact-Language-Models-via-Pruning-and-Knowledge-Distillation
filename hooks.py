652512298731a9ae3c6a2d0d334e0f3e68d37a2c
Alper Balbay <itsc0508@gmail.com>
2024-08-22 08:27:11 +0300

    implemented the width pruning with custom forward hooks

---
+++ hooks.py
@@ -0,0 +1,139 @@
+import torch
+import torch.nn as nn
+
+# set up the initial hooks for all the corresponding layers
+from models import GPT, Block
+
+
+def delete_importance_attr(layer: nn.Module):
+    if hasattr(layer, "calculated_importance"):
+        del layer.calculated_importance
+
+
+def remove_all_forward_hooks(model: GPT):
+    if not isinstance(model, GPT):
+        raise NotImplementedError("Only GPT models are supported for now")
+
+    for module in model.modules():
+        if isinstance(module, Block):
+            for head in module.sa.heads:
+                head._forward_hooks.clear()
+
+                head.key._forward_hooks.clear()
+                head.value._forward_hooks.clear()
+                head.query._forward_hooks.clear()
+
+                delete_importance_attr(head)
+
+                delete_importance_attr(head.key)
+                delete_importance_attr(head.query)
+                delete_importance_attr(head.value)
+
+            module.ffwd.net[0]._forward_hooks.clear()
+            module.ln1._forward_hooks.clear()
+            module.sa._forward_hooks.clear()
+            module.sa.proj._forward_hooks.clear()
+            delete_importance_attr(module.ffwd.net[0])
+            delete_importance_attr(module.ln1)
+            delete_importance_attr(module.sa)
+            delete_importance_attr(module.sa.proj)
+
+
+def register_all_forward_hooks(model: GPT):
+    if not isinstance(model, GPT):
+        raise NotImplementedError("Only GPT models are supported for now")
+
+    num_blocks = 0
+    for module in model.modules():
+        if isinstance(module, Block):
+            num_blocks += 1
+            for head in module.sa.heads:
+                head.register_forward_hook(attn_head_importance_hook)
+
+                head.key.register_forward_hook(neuron_importance_hook)
+                head.value.register_forward_hook(neuron_importance_hook)
+                head.query.register_forward_hook(neuron_importance_hook)
+
+            module.ffwd.net[0].register_forward_hook(
+                neuron_importance_hook
+            )  # register the forward hook to the linear layer inside of the ffwd block
+            module.sa.proj.register_forward_hook(neuron_importance_hook)
+            module.ln1.register_forward_hook(embedding_importance_hook)
+            module.register_forward_hook(block_importance_hook)
+
+
+def attn_head_importance_hook(
+    module, ins, outs
+) -> None:  # TODO: does the importance calculation returns the correct values for each head?
+    """calculates the multi-head-attention layer's importance per head"""
+    # outs.shape = (B, T, E) where B: batch_size, T: num tokens, E: embedding size
+    # the importance is calculated as summing the L2 norm of the attn outputs on B and T dimensions
+    outs_flat = outs.view(-1, outs.shape[-1])  # (b,t,e) -> (b*t, e)
+    importance = torch.linalg.vector_norm(outs_flat.detach().cpu(), ord=2, dim=-1).sum()
+
+    module.calculated_importance = importance
+
+    # print(outs_flat.shape)
+    # print("module:", module.__class__.__name__, end=" ")
+    # print("importance:", importance)
+    # print(f"{module.__class__.__name__} importance: {importance.shape}")
+
+
+def neuron_importance_hook(module, ins, outs) -> None:
+    """calculates the neuron importance for the given layer"""
+
+    # the ffwd linear weights should be in the shape of (out, in)
+    # the paper sums up the values of (X * W_i^T) meaning (B, T, in) x (in, 1)= (B,T,1) -> (1, ) (summed up)
+
+    # thus, in order to vectorize this operation, we'll need to hook this function to the first linear layer itself rather than the whole ffwd block.
+
+    # for each neuron in the ffwd layer, we can simply sum up the output columns
+
+    # as they're the activations of individual neurons
+    # calculate the importances
+    # importance = outs.detach().sum()
+    importance = outs.detach().cpu().sum(dim=(0, 1))
+    # print(f"{module.__class__.__name__} importance.shape: {importance.shape}")
+
+    module.calculated_importance = importance
+
+
+def embedding_importance_hook(module, ins, outs) -> None:
+    # the first block's first processing layer will be the
+    # layer norm
+    # so we'll just sum up the layer norm outputs after getting them
+    # calculate the importances
+
+    importance = outs.detach().sum(dim=(0, 1))
+    # print("importance.shape:", importance.shape)
+    # print("n_embd: ", outs.size(-1))
+    # print("module:", module.__class__.__name__)
+    # print("outs.shape:", outs.shape) # probably (B, T, E)
+
+    module.calculated_importance = importance
+
+    # print(f"{module.__class__.__name__} importance.shape: {importance.shape}")
+
+
+def block_importance_hook(module, ins, outs) -> None:
+
+    in_vectors = ins[0].detach()  # (B, T, E)
+    out_vectors = outs.detach()  # (B, T, E)
+
+    # Calculate cosine similarity for each sample and time step
+    dot_product = torch.sum(in_vectors * out_vectors, dim=-1)  # (B, T)
+    in_norm = torch.norm(in_vectors, p=2, dim=-1)  # (B, T)
+    out_norm = torch.norm(out_vectors, p=2, dim=-1)  # (B, T)
+
+    cosine_sim = dot_product / (in_norm * out_norm + 1e-8)  # (B, T)
+
+    # Calculate BI by taking the expectation (mean) and subtracting from 1
+    block_importance = 1 - torch.mean(cosine_sim)
+
+    # print("Block Importance:", block_importance.item())
+    # print("module:", module.__class__.__name__)
+    # print("outs.shape:", outs.shape)  # (B, T, E)
+
+    module.calculated_importance = block_importance
+
+    # print(f"{module.__class__.__name__} importance.shape: {block_importance.shape}")
---
+++ pruners.py
@@ -0,0 +1,202 @@
+import torch.nn as nn
+
+from models import Block
+
+
+def prune_neurons(model, ratio=0.2) -> None:
+    # goal: trim the MLP layer weights
+    # 1 - argsort the importances of the `ffwd` layers defined in the model
+    # 2 - remove the weights with respect to the given ratio
+    for module in model.modules():
+        if isinstance(module, Block):
+            importances = module.ffwd.net[0].calculated_importance
+            num_neurons = int((1 - ratio) * importances.size(0))
+            idx = importances.argsort(descending=True)[:num_neurons]
+            # reinitialize the weights along with the layer
+            dense1 = module.ffwd.net[0]
+            dense2 = module.ffwd.net[2]
+
+            module.ffwd.net[0] = nn.Linear(dense1.in_features, num_neurons).to(
+                model.device
+            )  # weights.shape = (num_neurons, dense1.in_features)
+            module.ffwd.net[2] = nn.Linear(num_neurons, dense2.out_features).to(
+                model.device
+            )  # weights.shape = (dense2.out_features = emb)
+
+            # now we need to set the weights to the new layers.
+
+            dense1.weight.data = dense1.weight.data[idx, :]
+            dense1.bias.data = dense1.bias.data[idx]
+
+            dense2.weight.data = dense2.weight.data[idx, :]
+            dense2.bias.data = dense2.bias.data[idx]
+
+            module.ffwd.net[0].calculated_importance = importances[idx]
+            module.ffwd.net[2].calculated_importance = importances[idx]
+
+    return model
+
+
+def prune_heads(model, ratio=0.2) -> None:
+    # goal: trim the attention heads' layer weights using the same approach as the `prune_neurons`
+    for module in model.modules():
+        if isinstance(module, Block):
+            # now the multi-head attention
+            for head in module.sa.heads:
+                # key,value,query weight shape: (head_size, n_embd) # n_embd
+                k, v, q = head.key, head.value, head.query
+
+                key_importances = head.key.calculated_importance
+                value_importances = head.value.calculated_importance
+                query_importances = head.query.calculated_importance
+
+                num_neurons = int((1 - ratio) * key_importances.size(0))
+
+                k_idx = key_importances.argsort(descending=True)[:num_neurons]
+                v_idx = value_importances.argsort(descending=True)[:num_neurons]
+                q_idx = query_importances.argsort(descending=True)[:num_neurons]
+
+                head.key = nn.Linear(k.in_features, num_neurons, bias=False).to(
+                    model.device
+                )
+                head.value = nn.Linear(v.in_features, num_neurons, bias=False).to(
+                    model.device
+                )
+                head.query = nn.Linear(q.in_features, num_neurons, bias=False).to(
+                    model.device
+                )
+
+                head.key.weight.data = k.weight.data[
+                    k_idx, :
+                ]  # (head_size, num_dense_embd)
+                head.value.weight.data = v.weight.data[
+                    v_idx, :
+                ]  # (head_size, num_dense_embd)
+                head.query.weight.data = q.weight.data[
+                    q_idx, :
+                ]  # (head_size, num_dense_embd)
+
+                head.key.calculated_importance = key_importances[k_idx]
+                head.value.calculated_importance = value_importances[v_idx]
+                head.query.calculated_importance = query_importances[q_idx]
+
+                # TODO: only the weights in the embedding layers are prunned (1st strategy)
+                # TODO: need to follow the correct implementation from the paper (pruning every linear layer?)
+
+            proj = module.sa.proj
+            proj_importances = module.sa.proj.calculated_importance
+            num_neurons = (
+                int((1 - ratio) * key_importances.size(0)) * module.sa.num_heads
+            )
+            idx = proj_importances.argsort(descending=True)[:num_neurons]
+
+            module.sa.proj = nn.Linear(num_neurons, proj.out_features).to(model.device)
+
+            module.sa.proj.weight.data = proj.weight.data[:, idx]
+            module.sa.proj.bias.data = proj.bias.data
+
+            module.sa.proj.calculated_importance = proj_importances[idx]
+
+
+def prune_embeddings(model, ratio=0.2) -> None:
+    # goal: trim the embedding dimension of the weight matrices in MLP, MHA, and LayerNorm layers.
+
+    for module in model.modules():
+        if isinstance(module, Block):
+            # start with pruning the MLP layers
+            importances = module.ln1.calculated_importance
+
+            dense1 = module.ffwd.net[0]  # weights.shape = (emb, 4 * emb)
+            dense2 = module.ffwd.net[2]  # weights.shape = (4 * emb, emb)
+
+            num_dense_embd = int((1 - ratio) * dense1.in_features)
+            idx = importances.argsort(descending=True)[:num_dense_embd]
+
+            module.ffwd.net[0] = nn.Linear(num_dense_embd, dense1.out_features).to(
+                model.device
+            )  # weights.shape = (num_dense_embd, dense1.in_features)
+            module.ffwd.net[2] = nn.Linear(dense2.in_features, num_dense_embd).to(
+                model.device
+            )  # weights.shape = (dense2.out_features = emb)
+
+            module.ffwd.net[0].weight.data = dense1.weight.data[:, idx]
+            module.ffwd.net[0].bias.data = dense1.bias.data
+            module.ffwd.net[2].weight.data = dense2.weight.data[idx, :]
+            module.ffwd.net[2].bias.data = dense2.bias.data[idx]
+
+            # now the multi-head attention
+            for head in module.sa.heads:
+                # key,value,query weight shape: (head_size, n_embd) # n_embd
+                k, v, q = head.key, head.value, head.query
+
+                head.key = nn.Linear(num_dense_embd, k.out_features, bias=False).to(
+                    model.device
+                )
+                head.value = nn.Linear(num_dense_embd, v.out_features, bias=False).to(
+                    model.device
+                )
+                head.query = nn.Linear(num_dense_embd, q.out_features, bias=False).to(
+                    model.device
+                )
+
+                head.key.weight.data = k.weight.data[
+                    :, idx
+                ]  # (head_size, num_dense_embd)
+                head.value.weight.data = v.weight.data[
+                    :, idx
+                ]  # (head_size, num_dense_embd)
+                head.query.weight.data = q.weight.data[
+                    :, idx
+                ]  # (head_size, num_dense_embd)
+
+                head.key.calculated_importance = k.calculated_importance
+                head.value.calculated_importance = v.calculated_importance
+                head.query.calculated_importance = q.calculated_importance
+
+            ln1 = module.ln1
+            ln2 = module.ln2
+
+            module.ln1 = nn.LayerNorm(num_dense_embd).to(model.device)
+            module.ln1.weight.data = ln1.weight.data[idx]
+            module.ln1.bias.data = ln1.bias.data[idx]
+
+            module.ln2 = nn.LayerNorm(num_dense_embd).to(model.device)
+            module.ln2.weight.data = ln2.weight.data[idx]
+            module.ln2.bias.data = ln2.bias.data[idx]
+
+            proj = module.sa.proj
+            module.sa.proj = nn.Linear(proj.in_features, num_dense_embd).to(
+                model.device
+            )
+            module.sa.proj.weight.data = proj.weight.data[
+                idx, :
+            ]  # (num_dense_embd, n_embd)
+            module.sa.proj.bias.data = proj.bias.data[idx]
+
+            module.sa.proj.calculated_importance = proj.calculated_importance
+
+    temb_table = model.token_embedding_table
+    pemb_table = model.position_embedding_table
+
+    model.token_embedding_table = nn.Embedding(model.vocab_size, num_dense_embd).to(
+        model.device
+    )
+    model.position_embedding_table = nn.Embedding(model.block_size, num_dense_embd).to(
+        model.device
+    )
+
+    model.token_embedding_table.weight.data = temb_table.weight.data[:, idx]
+    model.position_embedding_table.weight.data = pemb_table.weight.data[:, idx]
+
+    lnf = model.ln_f
+    ln_head = model.ln_head
+
+    model.ln_f = nn.LayerNorm(num_dense_embd).to(model.device)
+    model.ln_head = nn.Linear(num_dense_embd, ln_head.out_features).to(model.device)
+
+    model.ln_f.weight.data = lnf.weight.data[idx]
+    model.ln_f.bias.data = lnf.bias.data[idx]
+    model.ln_head.weight.data = ln_head.weight.data[
+        :, idx
+    ]  # weight.shape = (vocab_size, embd)
+    model.ln_head.bias.data = ln_head.bias.data
